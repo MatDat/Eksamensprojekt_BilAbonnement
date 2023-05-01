34ca7d519a009679ee5ec9d40131b1a719f3ebe7
MatDat <math03t7@stud.kea.dk>
2023-05-01 13:03:14 +0200

    Initial commit

---
+++ src/main/java/com/example/eksamensprojekt_bilabonnement/EksamensprojektBilAbonnementApplication.java
@@ -0,0 +1,13 @@
+package com.example.eksamensprojekt_bilabonnement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EksamensprojektBilAbonnementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EksamensprojektBilAbonnementApplication.class, args);
+    }
+
+}
