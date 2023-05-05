cfa68b71ebd135c86f82dc480d26bd698dfb8ecb
EmailKolar <Emilaalund.nyboder@gmail.com>
2023-05-05 10:55:35 +0200

    knapper og controllere til forretningsudvikler tilføjet

--- src/main/java/com/example/eksamensprojekt_bilabonnement/Controller/ForretningsudviklerController.java
+++ src/main/java/com/example/eksamensprojekt_bilabonnement/Controller/ForretningsudviklerController.java
@@ -2,11 +2,24 @@ package com.example.eksamensprojekt_bilabonnement.Controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ForretningsudviklerController {
 
 
+    @PostMapping("/forretningsudviklerSide")
+    public String forretningsudviklerSide(){
+        return "forretningsudvikler/forretningsudviklerSide";
+    }
+
+    @PostMapping("/seBiler")
+    public String seBiler(){
+
+
+
+        return "forretningsudvikler/seBiler";
+    }
 
 
 
