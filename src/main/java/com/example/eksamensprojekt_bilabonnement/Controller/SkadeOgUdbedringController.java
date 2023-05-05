43bb8d27fa488f60669e4b3abcadcc9906e35f8b
LauritsNoergaard <laur8709@stud.kea.dk>
2023-05-05 12:32:08 +0200

    Rapport form U1 - Laurits

--- src/main/java/com/example/eksamensprojekt_bilabonnement/Controller/SkadeOgUdbedringController.java
+++ src/main/java/com/example/eksamensprojekt_bilabonnement/Controller/SkadeOgUdbedringController.java
@@ -5,6 +5,7 @@ import com.example.eksamensprojekt_bilabonnement.Service.BilService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -22,5 +23,10 @@ public class SkadeOgUdbedringController {
         List<Bil> biler = bilService.hentBiler();
         model.addAttribute("biler", biler);
         return "skadeOgUdbedring/bilerKlarTilRapport";
+    }
+
+    @GetMapping("rapportForm")
+    public String rapportForm() {
+        return "skadeOgUdbedring/rapportForm";
     }
 }
