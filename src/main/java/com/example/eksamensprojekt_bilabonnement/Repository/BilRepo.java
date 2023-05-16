e57e229846a7bca428885f501d1d25523bf38df2
MatDat <math03t7@stud.kea.dk>
2023-05-16 09:21:23 +0200

    h2 commit

--- src/main/java/com/example/eksamensprojekt_bilabonnement/Repository/BilRepo.java
+++ src/main/java/com/example/eksamensprojekt_bilabonnement/Repository/BilRepo.java
@@ -14,7 +14,11 @@ public class BilRepo {
     @Autowired
     JdbcTemplate template;
 
-
+    public List<Bil> hentBiler() {
+        String sql = "SELECT * FROM bilabonnementdb.bil WHERE bil_tilstand = 'RAPPORTKLAR'";
+        RowMapper<Bil> rowMapper = new BeanPropertyRowMapper<>(Bil.class);
+        return template.query(sql, rowMapper);
+    }
 
     public List<Bil> hentAlleBiler() {
         String sql = "SELECT * FROM bilabonnementDB.bil, bilabonnementDB.model, bilabonnementDB.maerke " +
@@ -29,20 +33,22 @@ public class BilRepo {
         RowMapper<Bil> rowMapper = new BeanPropertyRowMapper<>(Bil.class);
         return template.query(sql, rowMapper, tilstand);
     }
-    public List<Bil> hentBilerMedBraendstof(String braendstof) {
-        String sql = "SELECT * FROM bilabonnementDB.bil, bilabonnementDB.model, bilabonnementDB.maerke " +
-                "WHERE braendstof = ? AND bil.model_id = model.model_id AND model.maerke_id = maerke.maerke_id;";
-        RowMapper<Bil> rowMapper = new BeanPropertyRowMapper<>(Bil.class);
-        return template.query(sql, rowMapper, braendstof);
-    }
-
-
-
 
     public void opdaterBilTilstand(String bilTilstand, int vognnummer) {
         String sql = "UPDATE bil " +
                 "SET bil_tilstand = ? " +
                 "WHERE vognnummer = ?";
         template.update(sql, bilTilstand, vognnummer);
+    }
+
+
+    public boolean opretBil(Bil bil) {
+        String sql = "INSERT INTO bil (vognnummer, stelnummer,maerke_id, model_id, staalpris, registrerings_afgift, " +
+                "CO2_udledning, bil_tilstand) " +
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+        template.update(sql, bil.getVognnummer(), bil.getStelnummer(), bil.getMaerke_navn(), bil.getModel_id(), bil.getStaalpris(),
+                bil.getRegistrerings_afgift(), bil.getCO2_udledning(), bil.getBilTilstand());
+        return true;
     }
 }
