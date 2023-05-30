bfaf6e5b12e9627559d8f23bc30dd75b40108751
Nademtis <Mathias.99.madsen@gmail.com>
2023-05-30 12:20:40 +0200

    Fixed DB name in repo - Frikke

--- src/main/java/com/example/eksamensprojekt_bilabonnement/Repository/SkadeRepo.java
+++ src/main/java/com/example/eksamensprojekt_bilabonnement/Repository/SkadeRepo.java
@@ -18,14 +18,14 @@ public class SkadeRepo {
 
     public void tilfoejSkade(Skade skade) {
         //Tilføjer en skade til Databasen, som metoden får med som inputparameter
-        String sql = "INSERT INTO skade (beskrivelse, pris, skaderapport_id) " +
+        String sql = "INSERT INTO bilabonnementDB.skade (beskrivelse, pris, skaderapport_id) " +
                 "VALUES (?, ?, ?)";
         template.update(sql, skade.getBeskrivelse(), skade.getPris(), skade.getSkaderapport_id());
     }
 
     public void tilfoejSkadeRapport(Skaderapport skaderapport) {
         //Tilføjer en skaderapport til Databasen, som metoden får med som inputparameter
-        String sql = "INSERT INTO skaderapport (skaderapport_dato, kontrakt_id, bruger_id) " +
+        String sql = "INSERT INTO bilabonnementDB.skaderapport (skaderapport_dato, kontrakt_id, bruger_id) " +
                 "VALUES (?, ?, ?)";
         template.update(sql, skaderapport.getSkaderapport_dato(),
                 skaderapport.getKontrakt_id(), skaderapport.getBruger_id());
@@ -33,7 +33,7 @@ public class SkadeRepo {
 
     public List<Skaderapport> hentSkaderapporter() {
         //Henter hele skaderapport table, plotter det ind i Skaderapport objekter og returnerer en Liste
-        String sql = "SELECT * FROM skaderapport";
+        String sql = "SELECT * FROM bilabonnementDB.skaderapport";
         RowMapper<Skaderapport> rowMapper = new BeanPropertyRowMapper<>(Skaderapport.class);
         return template.query(sql, rowMapper);
     }
@@ -41,7 +41,7 @@ public class SkadeRepo {
     public List<Skaderapport> hentSkaderapporterMedSortering(String sortering) {
         //Henter hele skaderapport table, med given sortering i fra inputparameter,
         // plotter det ind i Skaderapport objekter og returnerer en Liste
-        String sql = "SELECT * FROM skaderapport ORDER BY " + sortering + " ASC";
+        String sql = "SELECT * FROM bilabonnementDB.skaderapport ORDER BY " + sortering + " ASC";
         RowMapper<Skaderapport> rowMapper = new BeanPropertyRowMapper<>(Skaderapport.class);
         return template.query(sql, rowMapper);
     }
@@ -49,14 +49,14 @@ public class SkadeRepo {
     public List<Skade> hentSkaderFraSkaderapportId(int skaderapport_id) {
         //henter og returnerer en liste af skader med et givent skaderapport_id fra inputparameteret
         //og returner en liste af Skade objekter
-        String sql = "SELECT * FROM skade WHERE skaderapport_id = ?";
+        String sql = "SELECT * FROM bilabonnementDB.skade WHERE skaderapport_id = ?";
         RowMapper<Skade> rowMapper = new BeanPropertyRowMapper<>(Skade.class);
         return template.query(sql, rowMapper, skaderapport_id);
     }
 
     public int hentSkaderapportIdFraKontraktId(int kontrakt_id) {
         //udfra et givent kontrakt_id fra inputparameteret vil denne metode returnere et skaderapport_id
-        String sql = "SELECT skaderapport_id FROM skaderapport WHERE kontrakt_id = ?";
+        String sql = "SELECT skaderapport_id FROM bilabonnementDB.skaderapport WHERE kontrakt_id = ?";
         return template.queryForObject(sql, Integer.class, kontrakt_id);
     }
 
@@ -66,7 +66,7 @@ public class SkadeRepo {
         // og så inde i en try catch prøve at printe det første element fra listen
         // hvis listen indeholder en eller flere skader vil der returnes true
         // hvis listen ikke indeholder noget så vil metoden catch en IndexOutOfBoundsException og returnere false
-        String sql = "SELECT * FROM skade WHERE skaderapport_id = ?";
+        String sql = "SELECT * FROM bilabonnementDB.skade WHERE skaderapport_id = ?";
         RowMapper<Skade> rowMapper = new BeanPropertyRowMapper(Skade.class);
 
         try {
